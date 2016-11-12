65bafc31537c4806f9624363eed78247aae5b1a3
Olivier El Mekki <olivier@el-mekki.com>
2016-11-12 13:25:19 +0100

    ADD config
    Parse config and make sure its in an usable state.

---
+++ config.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"flag"
+	"os"
+)
+
+type Config struct {
+	WatchMode   bool
+	DatabaseUrl string
+	SqlDirPath  string
+}
+
+/*
+ * Retrieve configuration from command line options
+ */
+func ( config *Config ) Parse() ( err error ) {
+	if err = config.checkWatchMode() ; err != nil { return err }
+	if err = config.parseDatabaseUrl() ; err != nil { return err }
+	if err = config.parseSqlDir() ; err != nil { return err }
+
+	return
+}
+
+/*
+ * Check if we should exit or keep watching for fs change
+ */
+func ( config *Config ) checkWatchMode() ( err error ) {
+	flag.BoolVar( &config.WatchMode, "w", false, "Keep watching for filesystem change" )
+	flag.Parse()
+
+	if config.WatchMode {
+	 return fmt.Errorf( "Watch mode is not supported for now" )
+	}
+
+	return
+}
+
+/*
+ * Retrieve database connection info
+ */
+func ( config *Config ) parseDatabaseUrl() ( err error ) {
+	config.DatabaseUrl = os.Getenv( "DATABASE_URL" )
+
+	if  len( config.DatabaseUrl ) == 0 {
+		return fmt.Errorf( "You must provide database connection information as DATABASE_URL" )
+	}
+
+	return
+}
+
+/*
+ * Retrieve sql source directory
+ */
+func ( config *Config ) parseSqlDir() ( err error ) {
+	if len( os.Args ) == 1 || ( config.WatchMode && len( os.Args ) == 2 ) {
+		return fmt.Errorf( "You must provide a sql directory" )
+	}
+
+	config.SqlDirPath = os.Args[ len( os.Args ) - 1 ]
+
+	return
+}
--- main.go
+++ main.go
@@ -1,10 +1,22 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
+var Cfg Config
+
 /*
  * Usage :
  *   DATABASE_URL=url pgrebase [-w] sql_dir/
  */
 func ParseConfig() {
+	if err := Cfg.Parse() ; err != nil {
+		fmt.Printf( "Error: %v\n", err )
+		Usage()
+		os.Exit(1)
+	}
 }
 
 /*
@@ -19,6 +31,37 @@ func ParseConfig() {
  *
  */
 func CheckSanity() {
+}
+
+/*
+ * Give user a chance to know what went wrong
+ */
+func Usage() {
+	usage := `
+USAGE:
+	DATABASE_URL=url pgrebase [-w] <sql_directory>
+
+PgRebase is a tool that allows you to easily handle your postgres codebase for
+functions, triggers and views.
+
+Your expected to provide a postgresql connection url as DATABASE_URL and
+a sql directory as <sql_directory>.
+
+<sql_directory> should be structured that way:
+	<sql_directory>/
+	├── functions/
+	├── triggers/
+	└── views/
+
+At least one of functions/triggers/views/ should exist.
+
+OPTIONS:
+	-w: enter watch mode.
+		In watch mode, pgrebase will keep watching for file changes and will
+		automatically reload your sql code when it happens.
+	`;
+
+	fmt.Println( usage )
 }
 
 /*
